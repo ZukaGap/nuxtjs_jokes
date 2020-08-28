eeb57f5e22435773973acb95c5622fede409c8c8
ZukaGap <zurabigaprindashvili@gmail.com>
2020-08-28 18:06:09 +0400

    jokes

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome To The Best Corny Dad Jokes Website</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Welcome To Dad Jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
